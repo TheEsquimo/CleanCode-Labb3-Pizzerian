d42b80c97fb651abe8a5b81d523eb665ce35fb18
Elvis <elvostudios@hotmail.com>
2020-12-07 14:29:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CleanCode_Labb3_Pizzerian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
